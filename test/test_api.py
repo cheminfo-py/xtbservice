6518bb4530de76048a06e958b25a69fbd8eb03f1
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2021-09-20 13:00:05 +0200

    chore: fix test

--- test/test_api.py
+++ test/test_api.py
@@ -9,7 +9,7 @@ def test_from_smiles():
     response = client.get("/ir?smiles=CCCC")
     assert response.status_code == 200
     d =  response.json() 
-    assert len(d['modes']) == 3 * 10 -5
+    assert len(d['modes']) == 3 * 16  -6
 
 # def test_from_molfile():
 #     ...
