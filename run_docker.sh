42a641fb16a200a84ccdba90ac3e362d65707a67
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2021-11-09 12:26:11 +0100

    fix: run script

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "connect to http://localhost:8091"
-docker run -d -p 8091:8091 -e PORT=8091 --rm --name=xtb-serviceinstance --security-opt=seccomp:unconfined  xtb-service 
+docker run -d -p 8091:8091 -e PORT=8091 -e WORKERS=1 -e OMP_NUM_THREADS=1 --rm --name=xtb-serviceinstance --security-opt=seccomp:unconfined  xtb-service
